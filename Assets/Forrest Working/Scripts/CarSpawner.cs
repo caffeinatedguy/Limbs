1699a2b47ed6170b109abcdfca4254fad421f4ab
forrestsmith08 <forrest.smith08@gmail.com>
2016-01-30 11:13:01 -0800

    Added ObjectPool and CarSpawner

---
+++ Assets/Forrest Working/Scripts/CarSpawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CarSpawner : MonoBehaviour {
+
+	//Holds the paths for the cars to follow
+	public GameObject[] paths;
+	//Minimum time between car spawns
+	public float minSpawnTime;
+	//Maximum time between car spawns
+	public float maxSpawnTime;
+	//Maximum amount of cars that can be alive from this spawner
+	public int maxCarsAlive;
+	//Car names
+	public string[] carTypes = {"Cop"};
+
+
+	// Use this for initialization
+	void Start () {
+		//ObjectPool.FindObjectOfType(
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
