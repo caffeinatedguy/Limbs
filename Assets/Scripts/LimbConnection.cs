4a6918706c6f8c0c4a41dc8e6baa95475dd9d2be
Shawn <feddas00@gmail.com>
2016-01-31 14:43:55 -0800

    Reconnect limb by gameobject reference

--- Assets/Scripts/LimbConnection.cs
+++ Assets/Scripts/LimbConnection.cs
@@ -1,11 +1,15 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 [RequireComponent(typeof(Rigidbody))]
 public class LimbConnection : MonoBehaviour
 {
     public class LimbCache
     {
+        public GameObject Limb { get; set; }
+
         // Transform properties
         public Vector3 Position { get; set; }
         public Quaternion Rotation { get; set; }
@@ -15,43 +19,54 @@ public class LimbConnection : MonoBehaviour
         public float BreakForce { get; set; }
     }
 
-    public CharacterJoint LimbJoints;
-
-    private GameObject Limbs;
+    public List<CharacterJoint> LimbJoints;
     
-    private LimbCache start = new LimbCache();
+    private List<LimbCache> startValues = new List<LimbCache>();
     
     void Awake()
     {
-        Debug.Log(LimbJoints.name);
-        start.Connectedbody = LimbJoints.connectedBody;
-        start.BreakForce = LimbJoints.breakForce;
+        //Debug.Log(LimbJoints.name);
 
-        Limbs = LimbJoints.gameObject;
-        start.Position = Limbs.transform.localPosition;
-        start.Rotation = Limbs.transform.localRotation;
+        LimbCache currentLimb = new LimbCache();
+        foreach (var limbJoint in LimbJoints)
+        {
+            currentLimb = new LimbCache();
+            currentLimb.Connectedbody = limbJoint.connectedBody;
+            currentLimb.BreakForce = limbJoint.breakForce;
+            currentLimb.Limb = limbJoint.gameObject;
+            currentLimb.Position = currentLimb.Limb.transform.localPosition;
+            currentLimb.Rotation = currentLimb.Limb.transform.localRotation;
+            startValues.Add(currentLimb);
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
         //Debug.Log(LimbJoints.name);
-        if (Input.GetButtonDown("Jump")) ReconnectLimb(Limbs);
+        if (Input.GetButtonDown("Jump")) ReconnectLimb(startValues[0].Limb);
     }
 
-    void ReconnectLimb(GameObject limbToConnect)
+    public void ReconnectLimb(GameObject limbToConnect)
+    {
+        var chosenLimb = startValues.Where(l => l.Limb == limbToConnect).FirstOrDefault();
+
+        if (chosenLimb != null)
+            ReconnectLimb(chosenLimb);
+    }
+
+    private void ReconnectLimb(LimbCache limbToConnect)
     {
         // don't connect multiple times
-        if (limbToConnect.GetComponent<CharacterJoint>() != null)
+        if (limbToConnect.Limb.GetComponent<CharacterJoint>() != null)
             return;
 
         // position arm before joint is created to ensure connection.
-        limbToConnect.transform.localPosition = start.Position;
-        limbToConnect.transform.localRotation = start.Rotation;
+        limbToConnect.Limb.transform.localPosition = limbToConnect.Position;
+        limbToConnect.Limb.transform.localRotation = limbToConnect.Rotation;
 
-        var joint = limbToConnect.gameObject.AddComponent<CharacterJoint>();
-        joint.connectedBody = start.Connectedbody;
-        joint.breakForce = start.BreakForce;
-        //Debug.Log(joint.name);
+        var joint = limbToConnect.Limb.AddComponent<CharacterJoint>();
+        joint.connectedBody = limbToConnect.Connectedbody;
+        joint.breakForce = limbToConnect.BreakForce;
     }
 }
