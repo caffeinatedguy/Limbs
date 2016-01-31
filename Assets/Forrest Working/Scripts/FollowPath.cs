5c8b173eed8a4b5c8b50249d5513a7e6539c4f66
forrestsmith08 <forrest.smith08@gmail.com>
2016-01-30 18:42:08 -0800

    Changed car movement to physics based

--- Assets/Forrest Working/Scripts/FollowPath.cs
+++ Assets/Forrest Working/Scripts/FollowPath.cs
@@ -3,6 +3,7 @@ using System.Collections;
 
 public class FollowPath : MonoBehaviour {
 
+	public float power = 3.0f;
 	public float speed = 5.0f;
 	public float turnRate = 1.0f;
 	//how close to the node until it will start moving towards next one
@@ -14,10 +15,15 @@ public class FollowPath : MonoBehaviour {
 	private Transform[] nodes;
 	private int nodeNumber = 0;
 	private Vector3 target;
+	private Rigidbody _rigidbody;
 
 
 	// Use this for initialization
 	void Start () {
+		nodes = path.GetComponentsInChildren<Transform> ();
+		target = nodes [nodeNumber].transform.position;
+		followingPath = true;
+		_rigidbody = GetComponent<Rigidbody> ();
 	}
 
 	//Initialize a new path and set it to follow
@@ -31,20 +37,64 @@ public class FollowPath : MonoBehaviour {
 	}
 	
 	// Update is called once per frame
-	void LateUpdate () {
-		if (followingPath && path != null) {
+	void Update () {
+			if(followingPath && path != null) {
 			//Check if you're at the end of the path
 			//Find desired direction towards node
 			Vector3 direction = target - transform.position;
 
 			//Find desired rotation
-			Quaternion targetRotation = Quaternion.LookRotation (direction, Vector3.up);
+			//Quaternion targetRotation = Quaternion.LookRotation (direction.normalized, Vector3.up);
 
 			//Slerp to it over time
-			transform.rotation = Quaternion.Lerp (transform.rotation, targetRotation, turnRate * Time.deltaTime);
+			//transform.rotation = Quaternion.Slerp (transform.rotation, targetRotation, turnRate * Time.deltaTime);
 
 			//Move in forward direction at speed
-			transform.Translate (new Vector3 (0, 0, speed * Time.deltaTime));
+			//transform.Translate (new Vector3 (0, 0, speed * Time.deltaTime));*/
+
+
+
+			Vector3 curspeed = new Vector3(_rigidbody.velocity.x, _rigidbody.velocity.y, _rigidbody.velocity.z);
+
+
+
+			if(curspeed.magnitude > speed)
+			{
+				curspeed = curspeed.normalized;
+				curspeed *= speed;
+			}
+			
+
+
+			float angleBetween = Vector3.Angle (transform.forward, (target - transform.position));
+			Vector3 normalDirection = Vector3.Cross(transform.forward, (target - transform.position));
+			//Debug.Log (Vector3.Dot (V3,transform.up) + " With angle " + angleBetween);
+
+			//Always Drive Forward
+			_rigidbody.AddForce (transform.forward * power);
+
+			//Find the projection of the total speed in the forward direction
+			float forwardSpeed = Vector3.Project (curspeed, transform.forward.normalized).sqrMagnitude;
+
+			//If angle between heading and desired heading is negative, torque left
+			if(Vector3.Dot (normalDirection,transform.up) > 0 && forwardSpeed > 1)
+			{
+				_rigidbody.AddTorque(Vector3.up * turnRate);
+			}
+
+			//If angle between heading and desired heading is positive, torque right
+			if(Vector3.Dot (normalDirection,transform.up) < 0 && forwardSpeed > 1)
+			{
+				//transform.Rotate(Vector3.up * -turnRate);
+				_rigidbody.AddTorque(Vector3.down * turnRate);
+			}
+
+			/*if(Input.GetKey(KeyCode.D))
+			{
+				_rigidbody.AddTorque(Vector3.up * turnRate);
+
+			}*/
+
 
 			//Check if node has been reached, then target next node if not
 			//If last node has been reached set following and path to false and pool yourself
