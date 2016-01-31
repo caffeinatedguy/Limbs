c9df39a0400148c745a01f81b449a0bc1dcab4ee
Gregory Chudecke <gregc@presence-labs.com>
2016-01-30 23:47:15 -0800

    Scene update

---
+++ Assets/Takohi/Examples/SimpleIK/FollowMouse.js
@@ -0,0 +1,10 @@
+﻿#pragma strict
+
+var depth: float;
+
+function Update () {
+	var curScreenPoint = new Vector3(Input.mousePosition.x, Input.mousePosition.y, depth - Camera.main.transform.position.z);
+    var curPosition = Camera.main.ScreenToWorldPoint(curScreenPoint);
+    curPosition.z = depth;
+    transform.position = curPosition;
+}
